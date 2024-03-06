5b5740ec6fcce2493fdd1fcbb6243c4349ba8919
Andrew Efimenko <anrybio@gmail.com>
2024-03-06 16:08:56 +0300

    Добавил возможность добавлять различные сегменты

--- Assets/Scripts/RunnerGame/ItemFactory.cs
+++ Assets/Scripts/RunnerGame/ItemFactory.cs
@@ -6,26 +6,21 @@ namespace RunnerGame
 {
     public class ItemFactory<T> : IAbstractFactory<T> where T: MonoBehaviour
     {
-        private readonly List<T> _segments;
+        private readonly List<T> _variants;
         private readonly ObjectPool<T> _pool;
-        public ItemFactory(List<T> segments, int defaultCapacity = 15)
+        public ItemFactory(List<T> variants, int defaultCapacity = 15)
         {
-            _segments = segments;
+            _variants = variants;
             _pool = new ObjectPool<T>(CreateSegment,
                 OnTakeSegmentFromPool,
                 OnReturnSegment,
                 OnDestroySegment,
                 true, defaultCapacity, defaultCapacity * 10);
         }
-        private T CreateSegment() => Object.Instantiate(_segments[0]);
-        private void OnTakeSegmentFromPool(T segment)
-        {
-            segment.gameObject.SetActive(true);
-        }
-        private void OnReturnSegment(T segment)
-        {
-            segment.gameObject.SetActive(false);
-        }
+        private T CreateSegment() =>
+            Object.Instantiate(_variants[Random.Range(0, _variants.Count)]);
+        private void OnTakeSegmentFromPool(T segment) => segment.gameObject.SetActive(true);
+        private void OnReturnSegment(T segment) => segment.gameObject.SetActive(false);
         private void OnDestroySegment(T segment) => Object.Destroy(segment.gameObject);
         public T Create() => _pool.Get();
         public void Release(T segment) => _pool.Release(segment);
